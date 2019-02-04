7743b65cceacc6d9a0284244eaae288a04fe87ce
visualstudio <visualstudio@230-PC24>
2019-02-04 17:00:05 -0500

    final commit

--- C0742279/js/main.js
+++ C0742279/js/main.js
@@ -1,4 +1,5 @@
 ﻿// Your code here!
-
+function peanut() {
     var date = new Date();
-        document.body.innerHTML = "<h1> Today the date is " + date + " </h1>";
+    document.body.innerHTML = "<h1> Today the date is " + date + " </h1>";
+}        
